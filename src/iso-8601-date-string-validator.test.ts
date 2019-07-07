e4497b91e8992b73d538feca7ec970dd48157e12
Bwca <i.m.bwca@gmail.com>
2019-07-07 10:10:19 +0100

    migrated source to typescript

--- src/iso-8601-date-string-validator.test.ts
+++ src/iso-8601-date-string-validator.test.ts
@@ -1,13 +1,12 @@
-const moment = require("moment");
-const isIsoDatestring = require("./iso-8601-date-string-validator");
+import * as moment from 'moment';
+import { isIsoDatestring } from './iso-8601-date-string-validator';
 
 /** add extra zeros to number if needed */
-function pad(string, max) {
-  string = string.toString();
-  return string.length < max ? pad("0" + string, max) : string;
+function pad(numberStr: string, max: number) {
+  return numberStr.length < max ? pad("0" + numberStr, max) : numberStr;
 }
 
-test(`expect all moment dates from 0001 up to year 10000 to be true`, () => {
+ test(`expect all moment dates from 0001 up to year 10000 to be true`, () => {
   const dateFormat = "YYYY-MM-DD";
   const startDate = `0001-01-01`;
   const endYear = 10000;
@@ -17,46 +16,41 @@ test(`expect all moment dates from 0001 up to year 10000 to be true`, () => {
     expect(isIsoDatestring(date.format(dateFormat))).toBe(true);
     date.add(1, "d");
   }
-});
+}); 
 
 test(`expect function to return false, if a valid date contains
 an invalid digit separator`, () => {
-  expect(isIsoDatestring("2019/01/01")).toBe(false);
-});
+    expect(isIsoDatestring("2019/01/01")).toBe(false);
+  });
 
 test(`expect function to return true, if valid date contains
 an different digit separator, which is provided as an argument`, () => {
-  expect(isIsoDatestring("2019/01/01", "/")).toBe(true);
-});
+    expect(isIsoDatestring("2019/01/01", "/")).toBe(true);
+  });
 
 test(`expect 29 February to validate on leap years and fail on 
 non-leap years from year 0001 to year 10000`, () => {
-  let year = 1;
-  const maxYear = 10000;
-  const digits = 4;
+    let year = 1;
+    const maxYear = 10000;
+    const digits = 4;
 
-  pad = (string, max) => {
-    string = string.toString();
-    return string.length < max ? pad("0" + string, max) : string;
-  };
-
-  while (year < maxYear) {
-    const decembetDate = `${pad(year, digits)}-02-29`;
-    if (!moment([year]).isLeapYear()) {
-      expect(isIsoDatestring(decembetDate)).toBe(false);
-    } else {
-      expect(isIsoDatestring(decembetDate)).toBe(true);
+    while (year < maxYear) {
+      const decembetDate = `${pad(year.toString(), digits)}-02-29`;
+      if (!moment([year]).isLeapYear()) {
+        expect(isIsoDatestring(decembetDate)).toBe(false);
+      } else {
+        expect(isIsoDatestring(decembetDate)).toBe(true);
+      }
+      year++;
     }
-    year++;
-  }
-});
+  });
 
 test(`expect only 1-12 months to pass validation, 13-99 to fail`, () => {
   let month = 1;
   const digits = 2;
 
   while (month < 100) {
-    const date = `2019-${pad(month, digits)}-01`;
+    const date = `2019-${pad(month.toString(), digits)}-01`;
 
     if (month < 13) {
       expect(isIsoDatestring(date)).toBe(true);
@@ -73,7 +67,7 @@ test(`expect all dates after 31st to always fail for all months`, () => {
   for (let month = 1; month < 13; month++) {
     let day = 32;
     while (day < 100) {
-      const date = `2019-${pad(month, maxDigits)}-${day}`;
+      const date = `2019-${pad(month.toString(), maxDigits)}-${day}`;
       expect(isIsoDatestring(date)).toBe(false);
       day++;
     }
@@ -82,10 +76,10 @@ test(`expect all dates after 31st to always fail for all months`, () => {
 
 test(`September, April, June and November have 30 days, 
 expect false when these months have 31st date`, () => {
-  const months = ["04", "06", "09", "11"];
+    const months = ["04", "06", "09", "11"];
 
-  months.forEach(m => expect(isIsoDatestring(`2019-${m}-31`)).toBe(false));
-});
+    months.forEach(m => expect(isIsoDatestring(`2019-${m}-31`)).toBe(false));
+  });
 
 test(`February cannot have 30+ days`, () => {
   expect(isIsoDatestring(`2019-02-30`)).toBe(false);
