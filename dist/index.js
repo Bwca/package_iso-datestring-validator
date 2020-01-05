cff7797520fade3fa3074b065bcc2a619a767f1f
epishevvv <i.m.bwca@gmail.com>
2020-01-05 22:33:20 +0300

    removed dist folder

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./is-valid-date/is-valid-date.function"));
-__export(require("./is-valid-iso-datestring/is-valid-iso-datestring.function"));
-__export(require("./is-valid-time/is-valid-time.function"));
-__export(require("./is-valid-year-month/is-valid-year-month.function"));
--- dist/is-valid-time/is-valid-time.function.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- *
- * @param time hh:mm:ss (optional)
- * @param s separator between hours, minutes and optional seconds
- */
-export declare function isValidTime(time: string, s?: string): boolean;
