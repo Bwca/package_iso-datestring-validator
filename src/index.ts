89676bcecd936f90f2b2454dfdcc597612d896ec
epishevvv <i.m.bwca@gmail.com>
2020-01-06 14:22:36 +0300

    removed webpack

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { isValidDate } from './is-valid-date/is-valid-date.function';
-export { isValidISODateString } from './is-valid-iso-datestring/is-valid-iso-datestring.function';
-export { isValidTime } from './is-valid-time/is-valid-time.function';
-export { isValidYearMonth } from './is-valid-year-month/is-valid-year-month.function';
+export * from './is-valid-date/is-valid-date.function';
+export * from './is-valid-iso-datestring/is-valid-iso-datestring.function';
+export * from './is-valid-time/is-valid-time.function';
+export * from './is-valid-year-month/is-valid-year-month.function';
