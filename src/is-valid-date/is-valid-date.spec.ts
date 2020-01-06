78a810e51f2c6b49fa2c38d17e58e6755f1bdadd
epishevvv <i.m.bwca@gmail.com>
2020-01-06 10:17:26 +0300

    fixed isValidDate tests

--- src/is-valid-date/is-valid-date.spec.ts
+++ src/is-valid-date/is-valid-date.spec.ts
@@ -8,89 +8,171 @@ const isValidDateAsyncWrapper = (date: string, s = '-') =>
   Promise.resolve(isValidDate(date, s));
 
 test(`isValidDate. expect all moment dates from 0001 up to year 5000 to be true. also expect all ISO strings to validate`, async () => {
+  // Arrange
   const dateFormat = 'YYYY-MM-DD';
-  const startDate = `0001-01-01`;
+  const startDate = '0001-01-01';
   const endYear = 5000;
   const date = moment(startDate, dateFormat);
-  const results = [];
+  const promisedResults: Array<Promise<boolean>> = [];
+  let results: boolean[];
 
+  // Act
   while (date.year() < endYear) {
-    results.push(isValidDateAsyncWrapper(date.format(dateFormat)));
+    promisedResults.push(isValidDateAsyncWrapper(date.format(dateFormat)));
     date.add(1, 'd');
   }
-  expect(await Promise.all(results)).not.toContain(false);
+  results = await Promise.all(promisedResults);
+
+  // Assert
+  expect(results).not.toContain(false);
+  expect(results).toMatchSnapshot();
 });
 
 test(`isValidDate. expect all moment dates from 5000 up to year 10000 to be true. also expect all ISO strings to validate`, async () => {
+  // Arrange
   const dateFormat = 'YYYY-MM-DD';
-  const startDate = `5000-01-01`;
+  const startDate = '5000-01-01';
   const endYear = 10000;
   const date = moment(startDate, dateFormat);
-  const results = [];
+  const promisedResults: Array<Promise<boolean>> = [];
+  let results: boolean[];
 
+  // Act
   while (date.year() < endYear) {
-    results.push(isValidDateAsyncWrapper(date.format(dateFormat)));
+    promisedResults.push(isValidDateAsyncWrapper(date.format(dateFormat)));
     date.add(1, 'd');
   }
-  expect(await Promise.all(results)).not.toContain(false);
+  results = await Promise.all(promisedResults);
+
+  // Assert
+  expect(results).not.toContain(false);
+  expect(results).toMatchSnapshot();
 });
 
 test(`isValidDate. expect function to return true, if passed an empty separator with a valid date`, () => {
-  expect(isValidDate('20190101', '')).toBe(true);
+  // Arrange
+  const date = '20190101';
+  const separator = '';
+  let result: boolean;
+
+  // Act
+  result = isValidDate(date, separator);
+
+  // Assert
+  expect(result).toBe(true);
 });
 
 test(`isValidDate. expect function to return false, if a valid date contains an invalid digit separator`, () => {
-  expect(isValidDate('2019/01/01')).toBe(false);
+  // Arrange
+  const date = '2019/01/01';
+  let result: boolean;
+
+  // Act
+  result = isValidDate(date);
+
+  // Assert
+  expect(result).toBe(false);
 });
 
 test(`isValidDate. expect function to return true, if valid date contains an different digit separator, which is provided as an argument`, () => {
-  expect(isValidDate('2019/01/01', '/')).toBe(true);
+  // Arrange
+  const date = '2019/01/01';
+  const separator = '/';
+  let result: boolean;
+
+  // Act
+  result = isValidDate(date, separator);
+
+  // Assert
+  expect(result).toBe(true);
 });
 
 test(`isValidDate. expect 29 February to validate on leap years and fail on non-leap years from year 0001 to year 10000`, () => {
+  // Arrange
   let year = 1;
   const maxYear = 10000;
   const digits = 4;
-  while (year < maxYear) {
+  const negativeChecks: boolean[] = [];
+  const positiveChecks: boolean[] = [];
+
+  // Act
+  do {
     const decembetDate = `${pad(year.toString(), digits)}-02-29`;
-    !moment([year]).isLeapYear()
-      ? expect(isValidDate(decembetDate)).toBe(false)
-      : expect(isValidDate(decembetDate)).toBe(true);
-    year++;
-  }
+    const isLeapYear = moment([year]).isLeapYear();
+    const result = isValidDate(decembetDate);
+    isLeapYear ? positiveChecks.push(result) : negativeChecks.push(result);
+  } while (++year < maxYear);
+
+  // Assert
+  expect(positiveChecks).not.toContain(false);
+  expect(negativeChecks).not.toContain(true);
+  expect(positiveChecks).toMatchSnapshot();
+  expect(negativeChecks).toMatchSnapshot();
 });
 
 test(`isValidDate. expect only 1-12 months to pass validation, 13-99 to fail`, () => {
+  // Arrange
   let month = 1;
   const digits = 2;
+  const negativeChecks: boolean[] = [];
+  const positiveChecks: boolean[] = [];
 
-  while (month < 100) {
+  // Act
+  do {
     const date = `2019-${pad(month.toString(), digits)}-01`;
-    month < 13
-      ? expect(isValidDate(date)).toBe(true)
-      : expect(isValidDate(date)).toBe(false);
-    month++;
-  }
+    const result = isValidDate(date);
+
+    month < 13 ? positiveChecks.push(result) : negativeChecks.push(result);
+  } while (++month < 100);
+
+  // Assert
+  expect(positiveChecks).not.toContain(false);
+  expect(negativeChecks).not.toContain(true);
+  expect(positiveChecks).toMatchSnapshot();
+  expect(negativeChecks).toMatchSnapshot();
 });
 
 test(`isValidDate. expect all dates after 31st to always fail for all months`, () => {
+  // Arrange
   const maxDigits = 2;
+  const results: boolean[] = [];
+
+  // Act
   for (let month = 1; month < 13; month++) {
     let day = 32;
-    while (day < 100) {
+    do {
       const date = `2019-${pad(month.toString(), maxDigits)}-${day}`;
-      expect(isValidDate(date)).toBe(false);
-      day++;
-    }
+      results.push(isValidDate(date));
+    } while (++day < 100);
   }
+
+  // Assert
+  expect(results).not.toContain(true);
+  expect(results).toMatchSnapshot();
 });
 
 test(`isValidDate. September, April, June and November have 30 days, expect false when these months have 31st date`, () => {
+  // Arrange
   const months = ['04', '06', '09', '11'];
-  months.forEach((m) => expect(isValidDate(`2019-${m}-31`)).toBe(false));
+  let results: boolean[];
+
+  // Act
+  results = months.map((m) => isValidDate(`2019-${m}-31`));
+
+  // Assert
+  expect(results).not.toContain(true);
+  expect(results).toMatchSnapshot();
 });
 
 test(`isValidDate. February cannot have 30+ days`, () => {
-  expect(isValidDate(`2019-02-30`)).toBe(false);
-  expect(isValidDate(`2019-02-31`)).toBe(false);
+  // Arrange
+  const nonExistantFebruaryDays = ['2019-02-30', '2019-02-31'];
+  let results: boolean[];
+
+  // Act
+  results = nonExistantFebruaryDays.map((i) => isValidDate(i));
+
+  // Assert
+  expect(results).not.toContain(true);
+  expect(results).toMatchSnapshot();
 });
