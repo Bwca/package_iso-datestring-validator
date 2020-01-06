a54ea67ed907fcf37270fe4b6a7598b3158c914a
epishevvv <i.m.bwca@gmail.com>
2020-01-06 11:50:11 +0300

    is-valid-year-month-test

--- src/is-valid-year-month/is-valid-year-month.spec.ts
+++ src/is-valid-year-month/is-valid-year-month.spec.ts
@@ -6,17 +6,37 @@ import { isValidYearMonth } from './is-valid-year-month.function';
 const maxDigits = 2;
 
 test(`isValidYearMonth. expect months 01-12 to pass, 00 and 13+ to fail`, () => {
+  // Arrange
+  const positiveResults: boolean[] = [];
+  const negativeResults: boolean[] = [];
+
+  // Act
   for (let month = 0; month < 100; month++) {
     const validationResult = isValidYearMonth(
       `2019-${pad(month.toString(), maxDigits)}`,
     );
     month > 0 && month < 13
-      ? expect(validationResult).toBe(true)
-      : expect(validationResult).toBe(false);
+      ? positiveResults.push(validationResult)
+      : negativeResults.push(validationResult);
   }
+
+  // Assert
+  expect(positiveResults).not.toContain(false);
+  expect(negativeResults).not.toContain(true);
 });
 
 test(`isValidYearMonth. expect YYYYMM to validate when passed empty separator, else to fail`, () => {
-  expect(isValidYearMonth(`201901`, '')).toBe(true);
-  expect(isValidYearMonth(`201901`)).toBe(false);
+  // Arrange
+  const date = '201901';
+  const separator = '';
+  let positiveResult: boolean;
+  let negativeResult: boolean;
+
+  // Act
+  positiveResult = isValidYearMonth(date, separator);
+  negativeResult = isValidYearMonth(date);
+
+  // Assert
+  expect(positiveResult).toBe(true);
+  expect(negativeResult).toBe(false);
 });
